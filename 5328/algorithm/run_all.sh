40bbd55aebebd87cb216a81c211b54c9e12d0928
LanceYang0146 <tiancaishentong007@gmail.com>
2025-10-24 20:19:40 +1100

    Delete 5328 directory

--- 5328/algorithm/run_all.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-# Example batch commands
-python train.py --dataset FashionMNIST0.3 --method forward-knownT --epochs 10
-python train.py --dataset FashionMNIST0.6 --method gce --epochs 10
-python train.py --dataset CIFAR --method forward-estT --epochs 15
